fe4360107e06a86dce2d2ceba1fa1cb7e751152a
lego1980 <ronaldayson@hotmail.com>
2019-10-01 23:17:30 -0700

    comment out consoles for audit

--- backend/utils/limitQuery.js
+++ backend/utils/limitQuery.js
@@ -1,6 +1,6 @@
 //https://stackoverflow.com/questions/5539955/how-to-paginate-with-mongoose-in-node-js
 limitQuery = (query) => {
-    console.log("limitQuery",query.limitPerPage)
+    //console.log("limitQuery",query.limitPerPage)
     let defaultLimit = 5;
     if (query.hasOwnProperty('limitPerPage')) {
         if (query.limitPerPage != '0' && !isNaN(query.limitPerPage)) {
--- backend/utils/pageQuery.js
+++ backend/utils/pageQuery.js
@@ -1,6 +1,6 @@
 //https://stackoverflow.com/questions/5539955/how-to-paginate-with-mongoose-in-node-js
 pageQuery = (query) => {
-    console.log("pageNo",query.pageNo)
+    //console.log("pageNo",query.pageNo)
     let pageNo = 1;
     if (query.hasOwnProperty('pageNo')) {
         if (query.pageNo != '0' && !isNaN(query.pageNo)) {
