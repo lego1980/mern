c8450862b841bf70ce54a60ef0a6b5aa53072fdc
lego1980 <ronaldayson@hotmail.com>
2019-10-03 22:30:15 -0700

    rename

---
+++ backend/routes/VersionApiRoute.js
@@ -0,0 +1,10 @@
+
+// core
+const express = require('express');
+const router = express.Router();
+//category routes
+const routes = require('./routes');
+const version = "1.0";
+//router use rules
+router.use('/'+version, routes);
+module.exports = router;
